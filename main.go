e2f5ecfbaad42c1178b4a8449de509f4cc0a1723
weakvip <weakvip@163.com>
2021-01-30 20:42:53 +0800

    Update main.go
    FIX BUG

--- main.go
+++ main.go
@@ -1,45 +1,75 @@
 /*
-用KEY-VALUE形式存储JSON配置配置信息
-传出时如果之前有则用之前存储的，如果没有则首先存储传入值再将传入值直接传出(即相同的KEY第二次以后的传出值都为第一次的传入值)
-浏览器访问：key为配置名称, value为JSON格式, 传入JSON属性数量不固定(目前只支持一层JSON)
-例如: http://127.0.0.1:13485/?config={"k1":"v1","k2":"v2"}&config1={"k":123}
-功能单一，也可能没有什么实用价值，仅作为练手题目实现方式之一
+分布式配置服务端
+数据为JSON格式, 相同KEY首次GET时保存为默认值, POST更新默认值
+URL表单内NAME为配置名称, VALUE为JSON格式, 传入JSON属性数量不固定(目前仅支持一层JSON)
+例如: http://localhost:8080/?conf={"k1":"v1","k2":"v2"}&conf1={"k":123}
+功能单一，没有什么实用价值仅作为练手
 */
 package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"reflect"
+	"strings"
 	"sync"
 )
 
 var kv sync.Map
 
-//para jk:key in url, jv:value in url form, must be json; return r:default value
-func getDefaultValue(jk string, jv []string) (r []string, e error) {
+//para key:name in url, val:value in url form, must be json; return r:default value
+//i.e if url=host:port?conf={"k1":"v1"} then key=conf, val={"k1":"v1"}
+func getDefaultValue(method string, key string, val []string) (r []string, e error) {
+	uMethod := strings.ToUpper(method)
+	if "GET" != uMethod && "POST" != uMethod {
+		return nil, fmt.Errorf("wrong para: optional first para GET or POST")
+	}
+
 	var f interface{}
 
-	for _, v := range jv {
+	for _, v := range val { //UNIQUE KEY, DO NOT SUPPORT ARRAY VAL
 		var b = []byte(v)
 		if err := json.Unmarshal(b, &f); err != nil {
-			e = fmt.Errorf("invalid format json:%s %s", v, err.Error())
+			e = fmt.Errorf("invalid format json:%s %v", v, err)
 			break
 		} else {
 			m := f.(map[string]interface{})
-			for k, v := range m {
-				if mv, ok := kv.LoadOrStore(k, v); ok {
-					switch reflect.TypeOf(mv).Kind() {
-					case reflect.String:
-						if v != mv {
-							m[k] = mv
+			if repo, ok := kv.Load(key); ok { //UPDATE CURRENT NODE
+				mrepo := repo.(map[string]interface{})
+				for k, v := range m {
+					if mv, ok := mrepo[k]; ok {
+						switch reflect.TypeOf(m[k]).Kind() {
+						case reflect.String:
+							if v != mv { //high performance?
+								if "GET" == uMethod { //REVERT
+									m[k] = mv
+								} else if "POST" == uMethod { //SET
+									mrepo[k] = v
+								}
+							}
+						case reflect.Struct: //for depth more than 1 better to be detailed
+							fallthrough
+						default: //bug?
+							if "GET" == uMethod { //REVERT
+								m[k] = mv
+							} else if "POST" == uMethod { //SET
+								mrepo[k] = v
+							}
 						}
-					default: //bug, for depth more than 1 better to be detailed
-						reflect.ValueOf(&v).Elem().Set(reflect.ValueOf(mv))
+					} else { //INSERT NEW NODE
+						reflect.ValueOf(&mv).Elem().Set(reflect.ValueOf(v))
+						mrepo[k] = reflect.ValueOf(&mv).Interface()
 					}
 				}
+			} else { //INSERT NEW NODE
+				mn := make(map[string]interface{}, len(m))
+				for k, v := range m {
+					mn[k] = reflect.ValueOf(v).Interface()
+				}
+				kv.Store(key, mn)
 			}
 
 			if b, err := json.Marshal(f); err != nil {
@@ -50,7 +80,7 @@ func getDefaultValue(jk string, jv []string) (r []string, e error) {
 		}
 	}
 
-	return r, e
+	return
 }
 
 func onRouteMain(w http.ResponseWriter, r *http.Request) {
@@ -59,7 +89,7 @@ func onRouteMain(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "wrong para: para should not be null!")
 	} else {
 		for k, v := range r.Form {
-			if i, err := getDefaultValue(k, v); err != nil {
+			if i, err := getDefaultValue(r.Method, k, v); err != nil {
 				fmt.Fprintln(w, err.Error())
 			} else {
 				for _, mv := range i {
@@ -70,14 +100,18 @@ func onRouteMain(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func onRouteSet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "to to soon...")
-}
-
 func main() {
-	http.HandleFunc("/", onRouteMain)   //GET
-	http.HandleFunc("/set", onRouteSet) //SET
-	if err := http.ListenAndServe(":13485", nil); err != nil {
-		log.Fatal("faile to open http service:", err.Error())
+	var host *string = flag.String("h", "0.0.0.0", "host")
+	var port *uint = flag.Uint("p", 8080, "port")
+
+	flag.Parse()
+
+	url := fmt.Sprintf("%s:%d", *host, *port)
+	log.Println("LISTEN URL:", url)
+
+	http.HandleFunc("/", onRouteMain)
+
+	if err := http.ListenAndServe(url, nil); err != nil {
+		log.Fatal("failed to open http service:", err.Error())
 	}
 }
